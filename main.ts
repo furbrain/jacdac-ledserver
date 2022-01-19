c49ef0dc138a3262e667b4d372ed8a6e7f4846ac
Phil Underwood <beardydoc@gmail.com>
2022-01-19 22:48:37 +0000

    initial commit

---
+++ main.ts
@@ -0,0 +1,12 @@
+namespace servers {
+    /**
+     * Start all microbit servers
+     */
+    //% blockId=jacdac_microbit_start_all
+    //% block="start all jacdac servers"
+    export function startAll() {
+        jacdac.startServer()
+        servers.ledServer.start()
+        })
+    }
+}
---
+++ solasLED.ts
@@ -0,0 +1,113 @@
+namespace servers {
+    // Service LED constants
+    export const SRV_LED = 0x1e3048f8
+
+    export const enum LedVariant { // uint8_t
+        //% block="through hole"
+        ThroughHole = 0x1,
+        //% block="smd"
+        SMD = 0x2,
+        //% block="power"
+        Power = 0x3,
+        //% block="bead"
+        Bead = 0x4,
+    }
+
+    export const enum LedCmd {
+        /**
+         * This has the same semantics as `set_status_light` in the control service.
+         *
+         * ```
+         * const [toRed, toGreen, toBlue, speed] = jdunpack<[number, number, number, number]>(buf, "u8 u8 u8 u8")
+         * ```
+         */
+        Animate = 0x80,
+    }
+
+    export const enum LedReg {
+        /**
+         * Constant uint16_t. If known, specifies the number of LEDs in parallel on this device.
+         *
+         * ```
+         * const [ledCount] = jdunpack<[number]>(buf, "u16")
+         * ```
+         */
+        LedCount = 0x183,
+
+        /**
+         * Constant nm uint16_t. If monochrome LED, specifies the wave length of the LED.
+         *
+         * ```
+         * const [waveLength] = jdunpack<[number]>(buf, "u16")
+         * ```
+         */
+        WaveLength = 0x181,
+
+        /**
+         * Constant Variant (uint8_t). The physical type of LED.
+         *
+         * ```
+         * const [variant] = jdunpack<[jacdac.LedVariant]>(buf, "u8")
+         * ```
+         */
+        Variant = 0x107,
+    }
+
+    export class LEDServer extends jacdac.Server {
+        variant: LedVariant = LEDVariant.SMD
+        constructor() {
+            super("led", SRV_LED)
+        }
+
+        public handlePacket(pkt: jacdac.JDPacket) {
+            // registers
+            this.handleRegValue(
+                 pkt,
+                 LedReg.MaxPower,
+                 "u16",
+                 20
+                ) 
+            this.handleRegValue(
+                 pkt,
+                 LedReg.LedCount,
+                 "u16",
+                 1
+                ) 
+            this.handleRegValue(
+                 pkt,
+                 LedReg.Wavelength,
+                 "u16",
+                 650
+                ) 
+            this.handleRegValue(
+                 pkt,
+                 LedReg.MaxPower,
+                 "u8",
+                 this.variant
+                ) 
+
+            // commands
+            switch (pkt.serviceCommand) {
+                case LedCmd.Animate:
+                    this.handleAnimateCommand(pkt)
+                    break
+                default:
+                    pkt.possiblyNotImplemented()
+                    break
+            }
+        }
+
+        private handleAnimateCommand(pkt: jacdac.JDPacket) {
+            const [red, green, blue] =
+                pkt.jdunpack<[number, number, number]>("u16 u16 u16")
+            if (red == 0) {
+                pins.LED.digitalWrite(false)
+            } else {
+                pins.LED.digitalWrite(true)
+            }
+        }
+    }
+
+    //% fixedInstance whenUsed block="buzzer"
+    export const ledServer = new LEDServer()
+}
